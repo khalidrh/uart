0c2d8357f3b69e2f65d8f33824798ec8b879d365
khalidrh <khalid.rnha1@gmail.com>
2024-11-25 17:14:39 +0200

    Update freq divider.v

--- freq divider.v
+++ freq divider.v
@@ -1,30 +1,23 @@
-module freq-div(
-input clk ,// input clk =100 Mhz
-input rst, // async reset
-output clkdiv // desired freq (9.6 Khz)
+module freq_div(
+    input clk,            // Input clock (100 MHz)
+    input rst,            // Asynchronous reset
+    output reg clkdiv     // Output clock (9.6 kHz)
 );
 
-parameter div=10417 // =100M/9.6K
-reg [13:0] cnt; //2^14 =16k > 10k
+    parameter div = 10417; // Divider value = 100M / 9.6k
+    reg [13:0] cnt;        // 14-bit counter to support the division factor
 
-always@(posedge clk or posedge rst) begin 
-
-if(rst) begin
-cnt<=0;
-clkdiv<=0;
-
-end
-// the idea is to count the specific number of periods that we calculated by dividing the input freq and the desired feq (10417) 
-// every 10417/2  periods we toggle the clkdiv , which is half the period , in order to get half period on '1' and the other hald on '0' 
-else begin 
-if(cnt==div/2-1) begin 
-clkdiv<=~clkdiv;
-cnt<=0;
-
-else
-cnt<=cnt+1;
-end
-
-end
-end
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            cnt <= 0;
+            clkdiv <= 0;
+        end else begin
+            if (cnt == div/2 - 1) begin
+                clkdiv <= ~clkdiv; // Toggle output clock
+                cnt <= 0;         // Reset counter
+            end else begin
+                cnt <= cnt + 1;   // Increment counter
+            end
+        end
+    end
 endmodule
